7c0cdb22e966ffcde6d1e7331ab91be9d30db953
Emily Lau <lauyingwen@gmail.com>
2018-05-24 23:31:46 +0800

    updated input_name

---
+++ Lessons/P3-Introduction-to-Python/Lesson5-Scripting_Practices/input_name.py
@@ -0,0 +1,6 @@
+"""
+This prompts the user to enter a name and then uses the input in a greeting.
+The input function takes in whatever the user types and stores it as a string.
+"""
+name = input('Enter a name: ')
+print('Hello', name.title())
